828df43729011a8bdf5cdb6460e3126742c603ea
Steven Schlansker <stevenschlansker@gmail.com>
2025-08-28 09:35:40 -0700

    output

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ echo 'Build second run without tests, to compare'
 mvn clean install -DskipTests
 mv target/spring-boot-aot-reproducible*-tests.jar tests-without-aot.jar
 
+echo
+echo
+echo 'Diff between two runs:'
 diff <(jar tf tests-with-aot.jar) <(jar tf tests-without-aot.jar)
